081c98badb11f319a665b020a307efbfe476c18e
Rebecca Ariss <rebeccamarionruth@gmail.com>
2023-09-09 17:32:15 -0400

    Implement conditional rendering

--- src/components/Match.js
+++ src/components/Match.js
@@ -5,10 +5,7 @@ function Match(props) {
   return (
     <article className="Match">
       <h1>{players[0]} <span>vs</span> {players[1]}</h1>
-      {/* To be shown when there is a winner: */}
-      <h2>{winner} is the winner by {scoreDifference}!</h2>
-      {/* To be shown when there is no winner: */}
-      <h2>No winners yet!</h2>
+      {winner ? <h2>{winner} is the winner by {scoreDifference}!</h2> : <h2>No winners yet!</h2>}
     </article>
   );
 }
--- src/components/Player.js
+++ src/components/Player.js
@@ -1,16 +1,14 @@
 function Player(props) {
 
   const { gamerTag, firstName, lastName, wins } = props;
-
   return (
     <article className="Player">
       <h1>{firstName} <span>{gamerTag}</span> {lastName}</h1>
-      {/* To be shown when there are no wins */}
-      <h2 className="zero">Currently with no wins :(</h2>
-      {/* To be shown when there is 1 win */}
-      <h2>Currently at 1 win</h2>
-      {/* To be shown when there is more than one win */}
-      <h2>Currently at {wins} wins</h2>
+      {/* This is short circuit evaluation – if the condition left of '&&' resolves to
+      false, the h2 will not be rendered at all: */}
+      {wins === 0 && <h2 className="zero">Currently with no wins :(</h2>}
+      {wins === 1 && <h2>Currently at 1 win</h2>}
+      {wins > 1 && <h2>Currently at {wins} wins</h2>}
     </article>
   );
 }
